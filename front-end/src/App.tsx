f149c6f5cbab346cb72e18cf781443a96f577e8e
Michael <michaelolivares123@gmail.com>
2021-06-26 11:25:40 -0500

    fix: public folder fix

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,30 +3,14 @@ import { QueryClient } from 'react-query';
 import { QueryClientProvider } from 'react-query';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/Home.page";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <ToastContainer />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HomePage />
     </QueryClientProvider>
   );
 }
---
+++ front-end/src/pages/Home.page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface HomePageProps {
+  
+}
+ 
+const HomePage = () => {
+  return ( 
+    <div className="contaier">
+      <h1>Esto es la home page</h1>
+    </div>
+   );
+}
+ 
+export default HomePage;
