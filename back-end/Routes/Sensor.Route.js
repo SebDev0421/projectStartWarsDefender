f0b8012dcd8e61cd42ceaf17c3e994c9cc415f42
KevinSizzAL <kguzman@yourappland.com>
2021-06-26 12:35:32 -0500

    feat (backend): Sensors route

--- back-end/Routes/Sensor.Route.js
+++ back-end/Routes/Sensor.Route.js
@@ -1,13 +1,14 @@
-'use strict'
+"use strict";
 
-const express = require('express'),
-      app = express.Router();
+const express = require("express"),
+	app = express.Router();
 
+app.get("/", (req, res) => {
+	res.json({ status: "online" });
+});
 
+app.get("/", (req, res) => {
+	res.json({ status: "online" });
+});
 
-app.get('/',(req,res)=>{
-	res.json({'status':'online'})
-})
-
-
-module.exports = app
+module.exports = app;
