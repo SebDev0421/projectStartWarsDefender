06f712d65cd7a40b8150b805cc8e3a7f11210bb4
@sebDev0421 <juanse0421@gmail.com>
2021-06-26 11:24:09 -0500

    feat (bakend):create server

---
+++ back-end/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+
+const express = require('express'),
+      app = express(),
+      port = (process.env.PORT || 3000),
+      morgan = require('morgan');
+
+
+app.set('port',port);
+
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({extended:false}))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.use(express.json())
+
+app.listen(app.get('port'),()=>{
+	console.log('Server running in port : ',app.get('port'))
+})
