09785a42fc4d6ea0fdf9ac7c69671866c51282c0
@sebDev0421 <juanse0421@gmail.com>
2021-06-27 12:15:42 -0500

    fix(bakend):reapir alerts detect

--- back-end/components/sensors/network.js
+++ back-end/components/sensors/network.js
@@ -18,7 +18,7 @@ const {socket} = require("../../socket")
 const getDataMiddleware = (req, res, next) => {
 	const { data: sensors } = req.body;
 	const { data, date, originalData } = sensorsDataParser(sensors);
-	console.log("Body", data, date, originalData);
+	//console.log("Body", data, date, originalData);
 	req.data = data;
 	req.date = date;
 	req.originalData = originalData;
@@ -31,19 +31,19 @@ router.get("/", getDataMiddleware, (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-	const { data } = req.body;
+	const  {payload:data}  = req.body;
 	const { data:parsedSensorrsData } = req
 	//const { success} = await controller.saveDataSensors(originalData, date);
 	const parsed = { data: [], date: ""};
 
 	
 	sample += 1;
-
+	//console.log(data)
 	console.log(sample)
 	const parseout = await splitter(data);
 	const timeShow = parseout[0];
 
-	socket.io.emit("data", {data: data});
+	//socket.io.emit("data", {data: data});
 	const { success} = await controller.saveDataSensors(data, timeShow);
 	
 	clusters = []
@@ -102,9 +102,9 @@ router.post("/", async (req, res) => {
 
         	} else if (sensorValue === 0) {
 				//detect fail in sensor
-
+				//console.log('detect type 2 ',sensorName)
 				//entry in monitoring 2 seconds
-				sensorsAlerts.push({timeShow, type: "2", index, sensorValue, sensorName,smaple: sample+5})
+				sensorsAlerts.push({timeShow, type: "2", index, sensorValue, sensorName,smaple: sample+3})
 				//save alerts
 				//send index
 				const alerts = new Alerts({date: timeShow,sensor: index,value: sensorValue ,type:"2", ship:sensorName,information:"internal"})
@@ -118,8 +118,8 @@ router.post("/", async (req, res) => {
 				if(sensorSave[0].type === "1"){
 
 				if (sensorValue < 100){
-					console.log('alert canceled in sensor', index)
-					console.log('alert sensor value',sensorSave)
+					//console.log('alert canceled in sensor', index)
+					//console.log('alert sensor value',sensorSave)
 					sensorsAlerts= sensorsAlerts.filter((el,_index) => {
 						return(
 							el.index != index
@@ -134,7 +134,7 @@ router.post("/", async (req, res) => {
 				}}
 				else if(sensorSave[0].type === "2"){
 					if (sensorValue > 0){
-					console.log('alert canceled ', index)
+					//console.log('alert canceled ', index)
 					const alerts = new Alerts({date: timeShow,sensor: index,value: sensorValue ,type:"2", ship:sensorName,information:"cancelled"})
 				  await alerts.save()
 					sensorsAlerts= sensorsAlerts.filter((el,_index) => {
@@ -147,12 +147,12 @@ router.post("/", async (req, res) => {
 					return true;	
 				  }}
 				
-				if (sample === sensorSave[0].sample){
+				if (sample === sensorsAlerts[0].sample){
 					sendAlert(timeShow, sensorSave[0].type, index, sensorValue, sensorName)
-					console.log('send alert ',sensorSave[0].type)
-					console.log('send alert ',sensorSave[0].sensorValue)
-					console.log('send alert ',sensorSave[0].sensorName)
-					const alerts = new Alerts({date: timeShow,sensor: index,value: sensorValue ,type:"2", ship:sensorName,information:"Moff"})
+					//console.log('send alert ',sensorSave[0].type)
+					//console.log('send alert ',sensorSave[0].sensorValue)
+					//console.log('send alert ',sensorSave[0].sensorName)
+					const alerts = new Alerts({date: timeShow,sensor: index,value: sensorValue ,type:sensorSave[0].type, ship:sensorName,information:"Moff"})
 				    await alerts.save()
 					sensorsAlerts= sensorsAlerts.filter((el,_index) => {
 						return(
